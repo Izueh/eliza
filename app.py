59536c23eaf488024e1562d2a9a1d6a3a01ea2c6
Isaac Ramos <isaacd.ramosny@gmail.com>
2017-02-07 00:03:57 -0500

    fixed trailing slash issue.

--- app.py
+++ app.py
@@ -4,20 +4,18 @@ import datetime
 
 app = Flask(__name__)
 
-
-
 @app.route('/eliza', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
-        return render_template('index.html',name=request.form["name"], date=datetime.datetime.now().strftime("%b %d %Y %X"))
+        return render_template('index.html', name=request.form["name"], date=datetime.datetime.now().strftime("%b %d %Y %X"))
 
-
-@app.route('/eliza/DOCTOR', methods=['POST'])
+@app.route('/eliza/DOCTOR/', methods=['POST'])
+@app.route('/eliza/DOCTOR', methods=['POST'], )
 def doctor():
     if request.method == 'POST':
-        return jsonify(eliza=rive.reply('dev',request.form['human']))
+        return jsonify(eliza=rive.reply('dev', request.form['human']))
 
 
 if __name__ == '__main__':
--- static/js/chatform.js
+++ static/js/chatform.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         };
         $.ajax({
             type: "POST",
-            url: "/eliza/DOCTOR",
+            url: "/eliza/DOCTOR/",
             dataType: "json",
             data: request,
             success: function (data) {
